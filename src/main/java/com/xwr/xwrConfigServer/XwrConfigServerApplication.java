bba5f79ba637dc24a01d3aabebe7b9c58add5a39
yanghenglin <yanghl@xlyerp.com>
2021-02-17 10:09:25 +0800

    首次提交。

--- src/main/java/com/xwr/xwrConfigServer/XwrConfigServerApplication.java
+++ src/main/java/com/xwr/xwrConfigServer/XwrConfigServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient   // Eureka注册中心用到
 @RestController
 public class XwrConfigServerApplication {
-
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";//返回结果为字符串
+    }
     public static void main(String[] args) {
         SpringApplication.run(XwrConfigServerApplication.class, args);
     }
-
 }
